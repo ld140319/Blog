4dfb7c9f6f411a728ec9389fd3d88035b12d9328
zeming.liu <zeming.liu@toursforfun.cn>
2018-03-28 18:09:23 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /usr/local/nginx/html/Blog
-git pull origin master:master
-hexo clean
-hexo generate
-hexo deploy
+chmod 755 deploy.exp
+count=`ls themes/next|wc -w`
+if [ "$count" < "0" ];
+then
+ git clone https://github.com/iissnan/hexo-theme-next themes/next
+ fi
+ git pull origin master:master
+ hexo clean
+ hexo generate
+ hexo deploy

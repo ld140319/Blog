46bff43db778ca36e8ee222a304cf477b791bbd5
zeming.liu <zeming.liu@toursforfun.cn>
2018-03-28 22:11:30 +0800

    deply.sh change

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /usr/local/nginx/html/Blog
-chmod 755 deploy.exp
-count=`ls themes/next|wc -w`
+chmod 755 ls /usr/local/nginx/html/Blog/deploy.exp
+count=`ls /usr/local/nginx/html/Blog/themes/next/|wc -w`
 if [ $count -eq 0 ];
 then
- git clone https://github.com/iissnan/hexo-theme-next themes/next
+ git clone https://github.com/iissnan/hexo-theme-next  /usr/local/nginx/html/Blog/themes/next
  fi
 # git reset --hard HEAD
  git pull origin master:master -f

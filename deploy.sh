67d32c8551e45507d858aa47a1340ad589ae6cd4
zeming.liu <zeming.liu@toursforfun.cn>
2018-03-28 22:06:45 +0800

    deply.sh change

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 cd /usr/local/nginx/html/Blog
 chmod 755 deploy.exp
 count=`ls themes/next|wc -w`
-if [ "$count" < "0" ];
+if [ $count -eq 0 ];
 then
  git clone https://github.com/iissnan/hexo-theme-next themes/next
  fi
--- public/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo exec("cd /usr/local/nginx/html/Blog && git pull origin master:master && hexo clean && hexo generate && git deploy");
-?>

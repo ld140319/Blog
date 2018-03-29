3f12ba735cf567c2c89d081a5fe22a2110c12542
zeming.liu <zeming.liu@toursforfun.cn>
2018-03-29 10:03:24 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 cd /usr/local/nginx/html/Blog
-chmod 755  /usr/local/nginx/html/Blog/deploy.*
-if [ ! -d /usr/local/nginx/html/Blog/themes/next/ ]
-then
- mkdir -p /usr/local/nginx/html/Blog/themes/next/
-fi
-count=`ls /usr/local/nginx/html/Blog/themes/next/|wc -w`
-if [ $count -eq 0 ]
-then
- git clone https://github.com/iissnan/hexo-theme-next  /usr/local/nginx/html/Blog/themes/next
-fi
-# git reset --hard HEAD
-#git checkout -- /usr/local/nginx/html/Blog/public/*
-#git checkout -- /usr/local/nginx/html/Blog/deploy.*
-git checkout -- /usr/local/nginx/html/Blog/deploy.sh
-git checkout -- /usr/local/nginx/html/Blog/deploy.exp
-git pull origin master:master -f
-chmod 755  /usr/local/nginx/html/Blog/deploy.sh
-chmod 755  /usr/local/nginx/html/Blog/deploy.exp
 hexo clean
 hexo generate
 hexo deploy
---
+++ upload.sh
@@ -0,0 +1,19 @@
+#/usr/bin/bash
+cd /mnt/hgfs/www/Blog 
+dir="/mnt/hgfs/www/Blog/"
+git status|sed s/[[:space:]]//g >> lines.txt
+sed -i "s/#//g" lines.txt
+lines=`cat lines.txt`
+rm -f lines.txt
+for line in $lines
+do
+result=$(echo $line | grep "source/")
+if [[ "$result" != "" ]]
+then
+  file=${dir}${result}
+  if [ -e $file ]
+  then
+     scp $file root@118.24.47.193:/usr/local/nginx/html/Blog/source/_posts
+  fi
+fi
+done
